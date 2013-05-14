2d751da13a36f98a93e094cab77a78e39d6896f1
giobkboy <giobkboy@gmail.com>
2013-05-14 18:55:18 -0400

    you can add a note and you can select a note

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+import tkFont
 import re
 
 #notes by Giovanni Rescigno
@@ -26,52 +27,53 @@ class notebook:
         
         print "adding file", self.mainFile.name 
         #print self.maintext
-        
-        for self.text in self.maintext:
-             
-            Gui.listbox.insert(END, self.text[0:12])
-        
-        Gui.mainEntrynote.insert(END, self.maintext[0])
+
+        for self.text in self.maintext:#reads the text document
+            if self.text != self.maintext[0]:
+                Gui.listbox.insert(END, self.text[0:12])#puts i into the list box
             
-    def addnote(self):
-        
-        print "notebook.addnote clicked"
-        self.newnote = Gui.mainEntrynote.get(1.0, END)
-        self.maintext.append(self.newnote)
-        
-        self.refresh()
+        Gui.mainEntrynote.insert(END, self.maintext[0])#puts i in to the text document
         
         
     def refresh(self):
-        
+        print self.maintext
+        self.filewright = open("notebook.txt", "r+")
+        self.run = 0
         for self.textlist in self.maintext:
-            
-            self.filewright = open("notebook.txt", "wb+")
-            self.file.wright("<note>" + self.textlist)
-            
-        Gui.listbox.delete(0, END)
+            if self.run == 1:#make shre that the 1st index dose not get inclueded
+                self.filewright.write("<note>" + self.textlist) #wrights to the text document
+            self.run = 1
+           
+        
         self.filewright.close()
-        self.readnote()
+        self.mainFile = open("notebook.txt", "r+")
+        Gui.listbox.delete(0, END)
+        
         
         
 class main:
 
     def __init__(self):
         
-
+        
         self.listframe = Frame(root, height=400, width=250, bg = 'white')
         self.addframe = Frame(root, height=400, width=400, bg = "white")
+        self.menuframe = Frame(root, height=20, width=400, bg = "white")
         
+        self.menuframe.pack(side=TOP, fill=X)
         self.listframe.pack(side=LEFT, padx=5)
         self.addframe.pack(side=LEFT, fill=Y)
-        #self.label3.grid(columnspan=2, row=1, column=0)
-
+        
+        self.customFont = tkFont.Font(family="verdana", size=12)
+        
+        self.menu(self.menuframe)
         self.listnote(self.listframe)
         self.addnote(self.addframe)
+        
 
     def listnote(self, master): #add the selcter and button controles
 
-        self.spacer = Frame(master, height = 15)
+        self.spacer = Frame(master, height = 0)
         self.spacer.grid(row=0, column=0)
         
         self.notelist = Frame(master)
@@ -100,15 +102,15 @@ class main:
         self.distroy.pack(side=LEFT)
 
     def addnote(self, master):
-
+        
         self.topframe = Frame(master)
-        self.topframe.pack(side = TOP, padx=50, pady=8)
+        self.topframe.pack(side = TOP, padx=50, pady=2)
 
         self.midframe = Frame(master)
         self.midframe.pack(side=TOP, fill=Y, padx=5)
         
         self.mainEntrynote = Text(self.midframe, cursor="ibeam"
-                                   , width=45, height=27)
+                                   , width=45, height=27, font=self.customFont)
         self.mainEntrynote.pack(side=LEFT)
         
         self.textscr = Scrollbar(self.midframe)
@@ -120,25 +122,52 @@ class main:
         self.bottomframe = Frame(master)
         self.bottomframe.pack(side=TOP, fill = X)
         
-        self.save = Button(self.bottomframe, text="save", command=notebook.addnote)
+        self.save = Button(self.bottomframe, text="save", command=self.addnotetolist)
         self.save.pack(side = RIGHT, padx = 5)
         
-    def listhandler(self, event):
+    def listhandler(self, event):#if the list box is clicked
         
-        self.lisboxliss = event.widget
+        self.lisboxliss = event.widget #find the index of that list box
         self.index = int(self.lisboxliss.curselection()[0])
         Gui.mainEntrynote.delete(1.0, END) 
-        Gui.mainEntrynote.insert(END, notebook.maintext[self.index]) 
+        Gui.mainEntrynote.insert(END, notebook.maintext[self.index+1]) #corasponds to a index on a list
         
-    def createNewNote(self):
+    def createNewNote(self):#opens a new note
         
         self.mainEntrynote.delete(1.0, END)
         self.mainEntrynote.insert(END, "your notes here!")
         
+    def addnotetolist(self): #adds note to the list
+        
+        self.newnote = self.mainEntrynote.get(1.0, END)#geting the test form the entry
+        notebook.maintext.append(self.newnote)#appends it to the list of notes
+        
+        notebook.refresh()#runs the refresh which adds the note to the file
+        notebook.readnote()#open the note back up    
+        
+    def delete(self):
+        
+        return None
+    
+    def menu(self, master):
+        
+        self.master = master
+        self.menubar = Menu(self.master)
+        
+        self.fileMenu = Menu(self.menubar)
+        self.menubar.add_cascade(label="File", menu=self.fileMenu)
+        self.fileMenu.add_command(label="save")
+        
+        
+        root.config(menu=self.menubar)
+        
+        
+    
+        
     
         
 root = Tk()
-root.title("note book")
+root.title("notebook")
 root.resizable(FALSE,FALSE)
 
 Gui = main() 
